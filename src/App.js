2c0fd69046d3da6551de441d027a98b4cbcf4f70
uoohoon <hoonixox@naver.com>
2024-01-04 20:58:43 +0900

    feat: add post title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  let post = '강남 우동 맛집';
+  let [글제목1, b] = useState('남자 코트 추천')
+  let [글제목2, c] = useState('강남 우동 맛집')
+  let [글제목3, d] = useState('파이썬독학')
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>블로그임</h4>
+      </div>
+      <div className='list'>
+        <h4>{글제목1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목3}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
     </div>
   );
 }
