5a321592702c5e6e8c5fc9e3b9dbd281064b5d8a
uoohoon <hoonixox@naver.com>
2024-01-05 14:14:34 +0900

    refactor: refactor post title code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-// import logo from './logo.svg';
+/*eslint-disable*/
+import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   
-  let post = '강남 우동 맛집';
-  let [글제목1, b] = useState('남자 코트 추천')
-  let [글제목2, c] = useState('강남 우동 맛집')
-  let [글제목3, d] = useState('파이썬독학')
-  
+  let [글제목, b] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학'])
+
+
   return (
     <div className="App">
       <div className='black-nav'>
         <h4>블로그임</h4>
       </div>
       <div className='list'>
-        <h4>{글제목1}</h4>
+        <h4>{글제목[0]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{글제목2}</h4>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{글제목3}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
